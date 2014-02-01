e1fc2fad24cb18965bdb6ec7caad3d4e9f0cb3f8
Tomás Senart <tsenart@gmail.com>
2014-02-01 15:37:55 +0000

    Documentation

---
+++ bucket.go
@@ -0,0 +1,56 @@
+// Package throttle provides a generic lock-free implementation of the
+// Token Bucket algorithm where non-conformity is handled by the user.
+// http://en.wikipedia.org/wiki/Token_bucket
+
+package throttle
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+// Bucket defines a generic lock-free implementation of a Token Bucket.
+type Bucket struct {
+	ticker   *time.Ticker
+	capacity int64
+	tokens   int64
+}
+
+// NewBucket returns a new full Bucket with c capacity and asynchronously
+// starts filling it c times per second.
+func NewBucket(c int64) *Bucket {
+	b := &Bucket{time.NewTicker(time.Duration(1e9 / c)), c, c}
+	go b.fill()
+	return b
+}
+
+// Take will take n tokens out of the bucket. If there aren't enough
+// tokens, the difference is returned. Otherwise n is returned.
+// This method is thread-safe.
+func (b *Bucket) Take(n int64) int64 {
+	for {
+		if tokens := atomic.LoadInt64(&b.tokens); tokens == 0 {
+			return 0
+		} else if n <= tokens {
+			if !atomic.CompareAndSwapInt64(&b.tokens, tokens, tokens-n) {
+				continue
+			}
+			return n
+		} else if atomic.CompareAndSwapInt64(&b.tokens, tokens, 0) { // Spill
+			return tokens
+		}
+	}
+}
+
+// Stop halts the addition of new tokens to the buckets. It is not thread-safe.
+func (b Bucket) Stop() {
+	b.ticker.Stop()
+}
+
+func (b *Bucket) fill() {
+	for _ = range b.ticker.C {
+		if tokens := atomic.LoadInt64(&b.tokens); tokens < b.capacity {
+			atomic.AddInt64(&b.tokens, 1)
+		}
+	}
+}
---
+++ throttler.go
@@ -0,0 +1,59 @@
+// Package throttle provides a generic lock-free implementation of the
+// Token Bucket algorithm where non-conformity is handled by the user.
+// http://en.wikipedia.org/wiki/Token_bucket
+
+package throttle
+
+import (
+	"sync"
+)
+
+// DefaultThrottler is an utility instance of Throttler used by Throttle.
+var DefaultThrottler = &Throttler{buckets: map[string]*Bucket{}}
+
+// Throttle throttles a quantity 'in' to the specified 'rate' per second,
+// with a Bucket keyed by key, returning the permitted quantity.
+func Throttle(key string, in, rate int64) (out int64) {
+	return DefaultThrottler.Throttle(key, in, rate)
+}
+
+// Throttler is a thread-safe wrapper around a map of buckets and an easy to
+// use API for generic throttling.
+type Throttler struct {
+	mu      sync.RWMutex
+	buckets map[string]*Bucket
+}
+
+// NewThrottler returns an instance of Throttler
+func NewThrottler() *Throttler {
+	return &Throttler{buckets: map[string]*Bucket{}}
+}
+
+// Throttle throttles a quantity 'in' to the specified 'rate' per second,
+// with a Bucket keyed by key, returning the permitted quantity.
+// This method is thread-safe, locks are used only to synchronize access to
+// the bucket map.
+func (t *Throttler) Throttle(key string, in, rate int64) (out int64) {
+	t.mu.RLock()
+	b := t.buckets[key]
+	t.mu.RUnlock()
+
+	if b == nil {
+		b = NewBucket(rate)
+		t.mu.Lock()
+		t.buckets[key] = b
+		t.mu.Unlock()
+	}
+
+	return b.Take(in)
+}
+
+// Stop halts the addition of new tokens to all buckets. It is not thread-safe.
+func (t *Throttler) Stop() {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	for _, b := range t.buckets {
+		b.Stop()
+	}
+}
