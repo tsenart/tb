943bc9670bbb346d2ff8a82dc098686403b310cd
Tomás Senart <tsenart@gmail.com>
2014-02-01 16:02:17 +0000

    gdoc: Overview

--- bucket.go
+++ bucket.go
@@ -1,7 +1,3 @@
-// Package th provides a generic lock-free implementation of the
-// Token Bucket algorithm where non-conformity is handled by the user.
-// http://en.wikipedia.org/wiki/Token_bucket
-
 package th
 
 import (
---
+++ th.go
@@ -0,0 +1,6 @@
+// Package th provides a generic lock-free implementation of the
+// Token Bucket algorithm where non-conformity is handled by the user.
+// http://en.wikipedia.org/wiki/Token_bucket
+
+package th
+
