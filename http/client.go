9304e1d02e330a79832f6a355fe517dd02221457
Tomás Senart <tsenart@gmail.com>
2014-02-07 12:11:52 +0000

    http: Add ReqThrottledRoundTripper

--- http/client.go
+++ http/client.go
@@ -12,16 +12,32 @@ func (f roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
 	return f(r)
 }
 
-// ThrottledRoundTripper wraps another RoundTripper rt, throttling all requests
-// to the specified byte rate.
-func ThrottledRoundTripper(rt http.RoundTripper, rate int64) http.RoundTripper {
+// ByteThrottledRoundTripper wraps another RoundTripper rt,
+// throttling all requests to the specified byte rate.
+func ByteThrottledRoundTripper(rt http.RoundTripper, rate int64) http.RoundTripper {
 	hz := time.Duration(1 * time.Millisecond)
 	bucket := tb.NewBucket(rate, hz)
 
 	return roundTripperFunc(func(r *http.Request) (*http.Response, error) {
-		var got int64
+		got := bucket.Take(r.ContentLength)
 		for got < r.ContentLength {
 			got += bucket.Take(r.ContentLength - got)
+			time.Sleep(hz)
+		}
+		return rt.RoundTrip(r)
+	})
+}
+
+// ReqThrottledRoundTripper wraps another RoundTripper rt,
+// throttling all requests to the specified request rate.
+func ReqThrottledRoundTripper(rt http.RoundTripper, rate int64) http.RoundTripper {
+	hz := time.Duration(1e9 / rate)
+	bucket := tb.NewBucket(rate, hz)
+
+	return roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		got := bucket.Take(1)
+		for got != 1 {
+			got = bucket.Take(1)
 			time.Sleep(hz)
 		}
 		return rt.RoundTrip(r)
